01c69cdcbf1cf39e9d3e7b089c9814af38fd8511
21pxle <patricktanle@yahoo.com>
2020-01-15 17:10:53 -0500

    Added another method to move to infeed position

--- InfiniteRecharge2020/src/main/java/frc/robot/Commands/InfeedControl.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/Commands/InfeedControl.java
@@ -2,11 +2,19 @@
 package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class InfeedControl extends Command {
     private int inOrOut;
 
-    public FeederControl() {
+    public InfeedControl() {
         requires(Robot.infeed);
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
     }
 }
--- InfiniteRecharge2020/src/main/java/frc/robot/Subsystems/Infeed.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/Subsystems/Infeed.java
@@ -3,40 +3,39 @@ package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
-import frc.robot.Commands.XboxMove;
 
-import com.kauailabs.navx.frc.AHRS;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
  * The Infeed is a Subsystem that allows for the intake of lemons.
  */
 public class Infeed extends Subsystem {
 
-    TalonSRX lemonController;
-    TalonSRX drumMagMover;
-    
-    DoubleSolenoid armSolenoid1;
-    DoubleSolenoid armSolenoid2;
-    DoubleSolenoid armSolenoid3;
-    DoubleSolenoid armSolenoid4;
+    private TalonSRX lemonSqueezer;
+    private TalonSRX drumMagMover;
+
+    private DoubleSolenoid armSolenoid1;
+    private DoubleSolenoid armSolenoid2;
+    private DoubleSolenoid armSolenoid3;
+    private DoubleSolenoid armSolenoid4;
 
     public Infeed() {
-
         //Hey... when life gives you lemons, squeeze them to juice up those power ports!
-        lemonController = new TalonSRX(RobotMap.LEMON_SQUEEZER);
+        lemonSqueezer = new TalonSRX(RobotMap.LEMON_SQUEEZER);
 
+        //And move those drum magazines to squeeze even more!
         drumMagMover = new TalonSRX(RobotMap.DRUM_MAG_MOVER);
+
+        //Plus, you can never run out of those lemons if you can grab them using arms.
         armSolenoid1 = new DoubleSolenoid(RobotMap.ARM_SOLENOID_ONE_FORWARD, RobotMap.ARM_SOLENOID_ONE_REVERSE);
         armSolenoid2 = new DoubleSolenoid(RobotMap.ARM_SOLENOID_TWO_FORWARD, RobotMap.ARM_SOLENOID_TWO_REVERSE);
         armSolenoid3 = new DoubleSolenoid(RobotMap.ARM_SOLENOID_THREE_FORWARD, RobotMap.ARM_SOLENOID_THREE_REVERSE);
         armSolenoid4 = new DoubleSolenoid(RobotMap.ARM_SOLENOID_FOUR_FORWARD, RobotMap.ARM_SOLENOID_FOUR_REVERSE);
     }
-    
+
     //Left Trigger, Right Trigger
     public void extend(boolean retracting, boolean extending) {
         if (retracting) {
@@ -52,5 +51,16 @@ public class Infeed extends Subsystem {
             armSolenoid4.set(DoubleSolenoid.Value.kForward);
             SmartDashboard.putBoolean("Extended", false);
         }
+    }
+
+    public void moveToInfeedPosition() {
+        
+        //Fake variable and method for unknown drum mag subsystem - DNA (Do Not Activate)
+        drumMagMover.move(drumMagAngle);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        
     }
 }
