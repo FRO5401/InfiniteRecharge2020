8f57702282c838400c7246bf0d13dd8f1767045e
Slink789 <57464584+Slink789@users.noreply.github.com>
2020-02-05 19:31:34 -0500

    Ol + CompressorToggle

---
+++ InfiniteRecharge2020/src/main/java/frc/robot/Commands/CompressorToggle.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Add your docs here.
+ */
+public class CompressorToggle extends Command {
+
+  @Override
+  protected boolean isFinished() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+}
