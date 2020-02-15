850fda8ba08afb6e299389e0ff2fa96f2073f40f
IronKai123 <alirocks720@gmail.com>
2020-02-15 10:48:38 -0500

    report.

--- InfiniteRecharge2020/src/main/java/frc/robot/Robot.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/Robot.java
@@ -62,6 +62,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     Robot.drivebase.reportDriveBaseSensors();
+    Robot.lidar.reportLidarDistance();
   }
 
   /**
