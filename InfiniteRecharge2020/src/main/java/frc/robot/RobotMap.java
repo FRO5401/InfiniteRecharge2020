5400ff69ef4840acbeec773804905252acca6330
IronKai123 <alirocks720@gmail.com>
2021-07-28 15:18:22 -0400

    Removed Old Code

--- InfiniteRecharge2020/src/main/java/frc/robot/RobotMap.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/RobotMap.java
@@ -46,15 +46,6 @@ public class RobotMap {
   public static final double DRIVE_SENSITIVITY_DEFAULT = 1;
   public static final double SPIN_SENSITIVITY = 0.8;
 
-  // CarriageInfeed
-  public static final double CARRIAGE_FEEDER_SPEED = .4;
-  public static final double CARRIAGE_ANGLE_PER_PULSE = 0;
-
-  // Elevator
-  public static final int TIMEOUT_LIMIT_IN_Ms = 10;
-  public static final int ELEVATOR_THRESHOLD_FOR_PID = 0;
-  public static final double ELEVATOR_SPEED_SENSITIVITY = .4;
-
   /*** Operator Interfaces ***/
   // Controllers
   public static final int XBOX_CONTROLLER_DRIVER = 0;
@@ -90,12 +81,6 @@ public class RobotMap {
   public static final int DRIVE_MOTOR_LEFT_2 = 4;
   public static final int DRIVE_MOTOR_LEFT_3 = 6;
 
-  // CarriageInfeed
-  public static final int CARRIAGE_FEED_ROLLERS = 4;
-  public static final int CARRIAGE_TALON_CHANNEL = 2;
-  public static final int C_STOP_T = 7;
-
-
   /*** Solenoids (Single and Double) ***/
   // DoubleSolenoids have an IN and an OUT constant.
   // Solenoids have just one constant.
@@ -105,14 +90,6 @@ public class RobotMap {
   // DriveBase
   public static final int GEAR_SHIFTER = 0;
 
-  // Elevator
-  public static final int ELEVATOR_GEAR_SHIFTER = 4;
-  public static final int ELEVATOR_COLLAPSE_TOP = 5;
-  public static final int ELEVATOR_COLLAPSE_BOTTOM = 1;
-
-  // Hatch Mechanism
-  public static final int HATCH_EXTENDER = 2;
-
   /*** Sensors ***/
   // Encoders
   public static final int DRIVE_ENC_LEFT_A = 3;
@@ -120,13 +97,6 @@ public class RobotMap {
   public static final int DRIVE_ENC_LEFT_B = 4;
   public static final int DRIVE_ENC_RIGHT_B = 2;
 
-  // Elevator
-  public static final int E_STOP_HIGH = 5;
-  public static final int E_STOP_LOW = 6;
-
-  // DOUBLE SOLENOIDS ELEVATOR
-  public static final int BOTTOM_ELEVATOR_OUT = 6;
-  public static final int BOTTOM_EVEVATOR_IN = 7;
 
   /*** Autonomous ***/
   public static final int ANGLE_THRESHOLD = 2;
